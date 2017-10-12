cee6a7829380422c7eeb9078a6824e516ae9394e
anish2454 <shenoy.anish.227@gmail.com>
2017-10-11 23:28:59 -0400

    Added Templates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ HW07 -- Do I Know You?
 2017 - 10 - 9
 """
 
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 import os
 
 app = Flask(__name__)
@@ -24,32 +24,47 @@ def auth():
     if 'username' in request.form:
         return request.form['username'] == 'admin' and request.form['password'] == 'password'
     #the only correct username is "admin" and the only correct password is "password"
-    return False 
-    
+    return False
+
+def diag_print():
+        print ("\n\n\n")
+        print "---Diag: flask obj---"
+        print (app)
+        print "---Diag: request obj---"
+        print request
+        print "---Diag: request.headers---"
+        print request.headers
+        print "---Diag: request.args---"
+        print request.args
+        print "---Diag: request.form---"
+        print request.form
+
 @app.route('/')
 def home():
+    diag_print()
     return checklogged() #renders welcome page if a 'username' key exists
 
 @app.route('/login', methods=['POST','GET']) #uses POST method for form submission
 def login():
+    diag_print()
     if auth():
-            session['username'] = 'admin' #sets the 'username' key to something so that it can be recognized    
+            session['username'] = 'admin' #sets the 'username' key to something so that it can be recognized
             return redirect(url_for('welcome')) #logs the user in
+    else:
+            flash("Your Username/Password was incorrect!") #Flashes an error message
     return checklogged()
     #return render_template('login.html')
 
 @app.route('/welcome') #update to check credentials
 def welcome():
+    diag_print()
     if session.has_key('username'): #renders welcome page if a 'username' key exists
-        return render_template('welcome.html')
+        return render_template('welcome.html', UN = "admin")
     return checklogged()
-    
-@app.route('/failed')
-def failed():
-    return render_template('failed.html')
 
 @app.route('/logout')
 def logout():
+    diag_print()
     session.clear() #clears the session to remove the 'username' key
     return redirect(url_for('home')) #redirects user back to homepage
 
